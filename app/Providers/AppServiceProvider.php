dd6f93d9135c6135d65cede3daddb4bd168eaa02
Veasey <clintvee@gmail.com>
2021-07-28 19:13:48 +0100

    force HTTPS on production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,7 @@ use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\Facades\View;
 use Auth;
+use Url;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -28,8 +29,14 @@ class AppServiceProvider extends ServiceProvider
     {
       Blade::component('Track', Track::class);
 
+      // all views to see current user
       \View::composer('*', function($view){
         $view->with('user', \Auth::user());
       });
+
+      // force https on production
+      if($this->app->environment('production')) {
+          \URL::forceScheme('https');
+      }
     }
 }
