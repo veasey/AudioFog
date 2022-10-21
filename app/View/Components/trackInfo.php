976496ebc1249a1b19285c6304f895cc0a326295
Veasey <clintvee@gmail.com>
2022-10-21 21:35:44 +0100

    remove type, rename classes

--- app/View/Components/trackInfo.php
+++ app/View/Components/trackInfo.php
@@ -4,7 +4,7 @@ namespace App\View\Components;
 
 use Illuminate\View\Component;
 
-class trackInfo extends Component
+class TrackInfo extends Component
 {
     /**
      * Create a new component instance.
